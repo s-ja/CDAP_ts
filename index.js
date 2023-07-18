90d0d5f8358ac4202519d7734704b82bb2a2bb2a
s-ja <dkstmdwl0615@naver.com>
2023-07-18 18:47:13 +0900

    1강 종료

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 let myName = "asj";
+let myUsers = ["an", "kim", "yun"];
+let testObject = { name: "asj" };
+let testName = "an";
+let typeTest = true;
+function testFunction(x) {
+    return x * 2;
+}
+// function testFunction(x: any): number | string {
+//   return x * 2;
+// }
+testFunction(123);
+let asj = [123, true];
+let jhon = { name: "123", age: "123" };
+class User {
+    constructor(name) {
+        this.name = name;
+    }
+}
--- index.ts
+++ index.ts
@@ -1,1 +1,39 @@
 let myName: string = "asj";
+
+let myUsers: string[] = ["an", "kim", "yun"];
+
+let testObject: { name?: string } = { name: "asj" };
+
+let testName: string | number = "an";
+// union type
+
+type TestType = string | number | boolean;
+
+let typeTest: TestType = true;
+
+function testFunction(x: number): number {
+  return x * 2;
+}
+// function testFunction(x: any): number | string {
+//   return x * 2;
+// }
+
+testFunction(123);
+
+type Member = [number, boolean];
+let asj: Member = [123, true];
+// tuple type
+
+type NewMember = {
+  name: string;
+  [key: string]: string;
+};
+
+let jhon: NewMember = { name: "123", age: "123" };
+
+class User {
+  name: string;
+  constructor(name: string) {
+    this.name = name;
+  }
+}
