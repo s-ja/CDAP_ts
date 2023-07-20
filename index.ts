1ddedd1d86c225cb77e4040870faf79e0f6bf32f
seungji <dkstmdwl0615@naver.com>
2023-07-20 17:25:22 +0900

    2강 타입스크립트 기본 타입 정리_완료

--- index.ts
+++ index.ts
@@ -37,3 +37,37 @@ class User {
     this.name = name;
   }
 }
+
+// 2강 예제
+// *Q1. 여러분의 이름, 나이, 출생지역을 변수로 각각 저장해봅시다.
+// 물론 타입도 알아서 지정해보십시오. 이건 쉬우니 답은 없습니다.
+
+let myLastName: string = "seung ji";
+let myAge: number = 28;
+let myBirthPlace = "gwangju";
+// 대부분의 경우 초기 기본값 설정에 따라 알아서 '쉴드'를 씌워주는 모양이다.
+
+// *Q2. 여러분이 가장 좋아하는 곡과 가수이름을 변수에 object 자료형으로 담아보십시오.
+// object 안엔 노래 제목과 가수이름이 들어가면 됩니다.
+// 근데 제목과 가수는 문자만 들어올 수 있어야합니다.
+
+let favSongAndArtist: { song: string; artist: string } = {
+  song: "vulture",
+  artist: "pendulum",
+};
+
+// *Q3. 다음과 같이 생긴 자료의 타입지정을 해보도록 합시다.
+// let project = {
+//   member : ['kim', 'park'],
+//   days : 30,
+//   started : true,
+// }
+// project 변수 우측에 적으면 됩니다.
+// member 안엔 문자로 가득한 array만 들어올 수 있고
+// days는 숫자, started는 true/false만 들어올 수 있습니다.
+
+let project: { member: string[]; days: number; started: boolean } = {
+  member: ["kim", "park"],
+  days: 30,
+  started: true,
+};
