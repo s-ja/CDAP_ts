76e42dc03620f8ee15f7d7c84e7c99921b9e5566
s-ja <dkstmdwl0615@naver.com>
2023-08-28 16:17:00 +0900

    part2 2강 학습 완료

--- Narrowing_plus.js
+++ Narrowing_plus.js
@@ -1,4 +1,20 @@
 function fnc(a) {
-    if (a && typeof a === 'string') {
+    if (a && typeof a === "string") {
+    }
+}
+function animal01(animal) {
+    if ("swim" in animal) {
+        animal.swim;
+    }
+    else if ("fly" in animal) {
+        animal.fly;
+    }
+}
+function vehicle(a) {
+    if (a.wheel === "4ea") {
+        console.log("car is " + a.color);
+    }
+    else if (a.wheel === "2ea") {
+        console.log("bike is " + a.color);
     }
 }
--- Narrowing_plus.ts
+++ Narrowing_plus.ts
@@ -1,5 +1,40 @@
-function fnc(a : string | undefined){
-    if(a && typeof a === 'string'){
-        
-    }
+function fnc(a?: string | undefined) {
+  if (a && typeof a === "string") {
+  }
+}
+
+type Fish = { swim: string };
+type Bird = { fly: string };
+
+function animal01(animal: Fish | Bird) {
+  if ("swim" in animal) {
+    animal.swim;
+  } else if ("fly" in animal) {
+    animal.fly;
+  }
+}
+
+// obj instanceof parentClass
+
+// let date = new Date();
+// if (date instanceof Date) {
+//   console.log(date);
+// }
+
+type Car01 = {
+  wheel: "4ea";
+  color: string;
+};
+
+type Bike01 = {
+  wheel: "2ea";
+  color: string;
+};
+
+function vehicle(a: Car01 | Bike01) {
+  if (a.wheel === "4ea") {
+    console.log("car is " + a.color);
+  } else if (a.wheel === "2ea") {
+    console.log("bike is " + a.color);
+  }
 }
