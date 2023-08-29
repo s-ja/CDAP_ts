00777578d864ae72f0117066803907f56c6d4133
s-ja <dkstmdwl0615@naver.com>
2023-08-29 22:13:46 +0900

    10강 학습 완료 : redux 에서의 typescript

--- react_typescript/src/App.tsx
+++ react_typescript/src/App.tsx
@@ -1,4 +1,7 @@
 import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Dispatch } from "redux";
+import { RootState, increment, decrement, incrementByAmount } from "./index";
 
 let a: string = "kim";
 
@@ -7,8 +10,29 @@ let a: string = "kim";
 function App() {
   let [user, setUser] = useState<string | number>("kim");
 
+  const 꺼내온거 = useSelector((state: RootState) => state);
+  const dispatch: Dispatch = useDispatch();
+
   return (
     <div className="App">
+      {꺼내온거.counter1.count}
+      <button
+        onClick={() => {
+          // dispatch({ type: "증가" });
+          dispatch(increment());
+        }}
+      >
+        +버튼
+      </button>
+      <button
+        onClick={() => {
+          // dispatch({ type: "감소" });
+          dispatch(decrement());
+        }}
+      >
+        -버튼
+      </button>
+
       <h4>Hello {a}</h4>
       <h4>Hello {user}</h4>
       <Profile name="asj" age="20" />
--- react_typescript/src/index.tsx
+++ react_typescript/src/index.tsx
@@ -1,14 +1,68 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { createSlice, configureStore, PayloadAction } from "@reduxjs/toolkit";
+
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+
+interface Counter {
+  count: number;
+  user: string;
+}
+
+const 초기값: Counter = { count: 0, user: "kim" };
+
+// function reducer(state = 초기값, action: { type: string }): Counter {
+//   if (action.type === "증가") {
+//     return { count: state.count + 1 };
+//   } else if (action.type === "감소") {
+//     return { count: state.count - 1 };
+//   } else {
+//     return 초기값;
+//   }
+// }
+
+// const store = createStore(reducer);
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: 초기값,
+  reducers: {
+    increment(state) {
+      state.count += 1;
+    },
+    decrement(state) {
+      state.count -= 1;
+    },
+    incrementByAmount(state, action: PayloadAction<number>) {
+      state.count += action.payload;
+    },
+  },
+});
+
+let store = configureStore({
+  reducer: {
+    counter1: counterSlice.reducer,
+  },
+});
+
+//state 타입을 export 해두는건데 나중에 쓸 데가 있음
+export type RootState = ReturnType<typeof store.getState>;
+
+//수정방법 만든거 export
+export let { increment, decrement, incrementByAmount } = counterSlice.actions;
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
