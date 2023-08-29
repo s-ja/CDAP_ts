833cdd0a13d16bebf2364f4c2ebb3cc18dac76e1
s-ja <dkstmdwl0615@naver.com>
2023-08-29 21:39:02 +0900

    9강 학습 완료 : react에서의 typescript

--- react_typescript/src/App.tsx
+++ react_typescript/src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+let a: string = "kim";
+
+// let box: JSX.Element = <div></div>;
 
 function App() {
+  let [user, setUser] = useState<string | number>("kim");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h4>Hello {a}</h4>
+      <h4>Hello {user}</h4>
+      <Profile name="asj" age="20" />
     </div>
   );
+}
+
+function Profile(props: { name: string; age: string }): JSX.Element {
+  return <div>{props.name}'s Profile</div>;
 }
 
 export default App;
